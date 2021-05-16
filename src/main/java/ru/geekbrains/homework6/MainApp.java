4b07e102f2ed2e359d0749f3ba71543c11ac1f2b
Антон <toni.golovkin.89@inbox.ru>
2021-05-16 18:40:21 +0300

    HomeWork6

--- src/main/java/ru/geekbrains/homework6/MainApp.java
+++ src/main/java/ru/geekbrains/homework6/MainApp.java
@@ -1,4 +1,16 @@
 package ru.geekbrains.homework6;
 
+import java.util.concurrent.Callable;
+
 public class MainApp {
+    public static void main(String[] args) {
+      Cat cat = new Cat("Пушок");
+      Dog dog = new Dog("Бобик");
+
+      cat.run(50);
+      cat.swim(5);
+      dog.run(150);
+      dog.swim(4);
+
+    }
 }
